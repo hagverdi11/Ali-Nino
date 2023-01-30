21778beb4a01dce9ee60aef42f0f450a2e7848a0
hagverdi11 <hagverdiam@code.edu.az>
2023-01-30 16:08:48 +0400

    Added footer

--- script.js
+++ script.js
@@ -12,4 +12,25 @@ for (var i = 0; i < btns.length; i++) {
 }
   
 
+// FAQ
+
+const accordionBtns = document.querySelectorAll(".item-header");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("active");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
 
